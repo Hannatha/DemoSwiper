aba112d89aa3617120488e370602ecfa5e9393e2
19007457 <19007457@myrp.edu.sg>
2021-06-01 10:34:44 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/demoswiper/Frag1.java
@@ -0,0 +1,22 @@
+package sg.edu.rp.webservices.demoswiper;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Frag1 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+
+        return view;
+    }
+}
